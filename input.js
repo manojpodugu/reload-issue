55a8710dcf8303fd8143f28b3ec1e86f706c8f8c
manojpodugu <psmk126@gmail.com>
2025-03-25 12:33:18 +0530

    Create input.js

---
+++ input.js
@@ -0,0 +1,11 @@
+function navigate() {
+    let source = document.getElementById("source").value;
+    let destination = document.getElementById("destination").value;
+    
+    // Store values in localStorage
+    localStorage.setItem("source", source);
+    localStorage.setItem("destination", destination);
+
+    // Redirect to map page
+    window.location.href = "map.html";
+}
